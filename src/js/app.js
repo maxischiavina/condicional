0936ab93d9e60a42ed92f5604618e37d1be8b61f
maxischiavina <maxischiavina@hotmail.com>
2023-03-31 19:10:53 +0000

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -23,19 +23,19 @@ import "../style/index.css";
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); //print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
-  if (variables.name == null) variables.name = "Nombre";
-  if (variables.lastname == null) variables.lastname = "Apellido";
-  if (variables.role == null) variables.role = "Role";
-  if (variables.city == null) variables.city = "City";
-  if (variables.country == null) variables.country = "Country";
+    console.log("These are the current variables: ", variables); //print on the console
+    // here we ask the logical questions to make decisions on how to build the html
+    // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+    let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+    if (variables.includeCover == false) cover = "<div class='cover'></div>";
+    if (variables.name == null) variables.name = "Nombre";
+    if (variables.lastname == null) variables.lastname = "Apellido";
+    if (variables.role == null) variables.role = "Role";
+    if (variables.city == null) variables.city = "City";
+    if (variables.country == null) variables.country = "Country";
 
-  // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
+    // reset the website body with the new html output
+    document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
           <h1>${variables.name} ${variables.lastname}</h1>
@@ -55,42 +55,42 @@ function render(variables = {}) {
  * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
 window.onload = function() {
-  window.variables = {
-    // if includeCover is true the algorithm should
-    includeCover: true,
-    // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
-    // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
-    // social media bar position (left or right)
-    socialMediaPosition: "position-left",
-    // social media usernames
-    twitter: null,
-    github: "alesanchezr",
-    linkedin: null,
-    instagram: null,
-    name: null,
-    lastname: null,
-    role: null,
-    country: null,
-    city: null
-  };
-  render(window.variables); //render the card for the first time
+    window.variables = {
+        // if includeCover is true the algorithm should
+        includeCover: true,
+        // this is the url of the image that will used as background for the profile cover
+        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+        // this is the url for the profile avatar
+        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+        // social media bar position (left or right)
+        socialMediaPosition: "position-left",
+        // social media usernames
+        twitter: null,
+        github: "alesanchezr",
+        linkedin: null,
+        instagram: null,
+        name: null,
+        lastname: null,
+        role: null,
+        country: null,
+        city: null
+    };
+    render(window.variables); //render the card for the first time
 
-  document.querySelectorAll(".picker").forEach(function(elm) {
-    elm.addEventListener("change", function(e) {
-      // <- add a listener to every input
-      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
-      let values = {};
-      values[attribute] =
-        this.value == "" || this.value == "null"
-          ? null
-          : this.value == "true"
-          ? true
-          : this.value == "false"
-          ? false
-          : this.value;
-      render(Object.assign(window.variables, values)); // render again the card with new valus
+    document.querySelectorAll(".picker").forEach(function(elm) {
+        elm.addEventListener("change", function(e) {
+            // <- add a listener to every input
+            const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
+            let values = {};
+            values[attribute] =
+                this.value == "" || this.value == "null" ?
+                null :
+                this.value == "true" ?
+                true :
+                this.value == "false" ?
+                false :
+                this.value;
+            render(Object.assign(window.variables, values)); // render again the card with new valus
+        });
     });
-  });
 };
